d9fa3c5c0d105af2eee83224d937dfa7d2c4b673
Harshit Saini <harshit.saini@widas.in>
2020-04-10 13:22:19 +0530

    Updates

---
+++ precache-manifest.a9ccbe9becb80a72298722f9e7cf39d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cabb8f959dcabd0da8b712f1e6daa4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bb6b3c21f69cc7f22f0f",
+    "url": "/monsters-rolodex/static/css/main.391c066d.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6b3c21f69cc7f22f0f",
+    "url": "/monsters-rolodex/static/js/main.b19a92f2.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
